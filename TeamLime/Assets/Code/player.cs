8d3a841232341111b344b760aeb0adb49a82bf67
Densch <dennis.schaffner@gmx.de>
2024-04-10 17:08:27 +0200

    [EDIT] NewInputsystem implementiert

--- TeamLime/Assets/Code/player.cs
+++ TeamLime/Assets/Code/player.cs
@@ -1,19 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float speed;
-    [SerializeField] private string inputNameHorizontal;
-    [SerializeField] private string inputNameVertical;
+    [SerializeField] private InputAction _moveAction;
+
+    [SerializeField] private PlayerInput _inputActions;
+    [SerializeField] private InputAction _interactAction;
 
     private Rigidbody2D rb;
+    private Vector2 moveInput;
 
 
-    private float inputHorizontal;
-    private float inputVertical;
+    private void Awake()
+    {
+        _interactAction = _inputActions.actions["Interact"];
+        _interactAction.performed += _ => OnInteract();
 
+    }
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -22,14 +29,21 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        inputHorizontal = Input.GetAxisRaw(inputNameHorizontal);
-        inputVertical = Input.GetAxisRaw(inputNameVertical);
 
     }
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(inputHorizontal * speed * Time.fixedDeltaTime, inputVertical * speed * Time.fixedDeltaTime);
+        Vector2 playerVelocity = new Vector2(moveInput.x * speed, moveInput.y * speed);
+        rb.velocity = playerVelocity;
     }
+    void OnInteract()
+    {
 
+    }
 }
