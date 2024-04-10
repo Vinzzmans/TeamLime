3d24cd74c3427123e19f1fc755bee5b5d0ad15f7
Vinzzman <vincentvonhartz@gmx.de>
2024-04-10 15:58:38 +0200

    [ADD] Player script, Mainscene blockout

---
+++ TeamLime/Assets/Code/player.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private string inputNameHorizontal;
+    [SerializeField] private string inputNameVertical;
+
+    private Rigidbody2D rb;
+
+
+    private float inputHorizontal;
+    private float inputVertical;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void Update()
+    {
+        inputHorizontal = Input.GetAxisRaw(inputNameHorizontal);
+        inputVertical = Input.GetAxisRaw(inputNameVertical);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(inputHorizontal * speed * Time.fixedDeltaTime, inputVertical * speed * Time.fixedDeltaTime);
+    }
+
+}
